a01e400f4b154730965e8eb0ef6efcee6e7f8c56
ElizetSolanoEspinoza <167382251+ElizetSolanoEspinoza@users.noreply.github.com>
2024-05-04 12:40:44 -0600

    Add files via upload

---
+++ encabezado.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<center><img width="750" height="220" src="https://epsenlinea.co/wp-content/uploads/2020/10/mi-seguridad-social.png"/>
+<h3 id="centrado">ENCUESTA DE INSEGURIDAD DEL CIUDADANO
+EN LA CIUDAD DE LIMA
+</h3>
+</body>
+</html>
---
+++ pie.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<h4 id="centrado">Todos los derechos reservados
+AlanVelazquez@2021</h4>
+<center><img width="120" height="120" src="http://www.kimicamerica.com/b8.gif"/>
+</body>
+</html>
